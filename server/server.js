0378a9f05d3ca8124abbb986c9488e0eefcf8172
Mads Mobæk <mads86@gmail.com>
2013-04-11 21:31:56 +0200

    Hacking backlog

--- server/server.js
+++ server/server.js
@@ -13,6 +13,12 @@ var startServer = function() {
 
     server.use(restify.bodyParser({mapParams:false}));
 
+    server.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        next();
+    });
+
     tasks.initialize(server);
     users.initialize(server);
     status.initialize(server);
