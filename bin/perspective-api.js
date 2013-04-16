5015647aaebd94a284c590b1bd52fb8b95819c30
Mads Mobæk <mads86@gmail.com>
2013-04-17 00:02:05 +0200

    Normalizing tabs -> 2 spaces

--- bin/perspective-api.js
+++ bin/perspective-api.js
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
 
 var Server = require('../lib/server'),
-	Db = require("../lib/db"),
-    services = require("../lib/services"),
-	config = require("../config.json"),
+  Db = require("../lib/db"),
+  services = require("../lib/services"),
+  config = require("../config.json"),
 
-    server = new Server(config.server),
-    database = new Db(config.db),
-    modules = []; 
+  server = new Server(config.server),
+  database = new Db(config.db),
+  modules = [];
 
 services.register("server", server);
 services.register("db", database);
 
 modules = config.modules.map(function(name) {
-    var module = require("../lib/" + name + "/" + name + ".js");
-    module.initialize();
-    return module;
+  var module = require("../lib/" + name + "/" + name + ".js");
+  module.initialize();
+  return module;
 });
 
 database.setup({
-    onSuccess: function() {
-        server.start(modules);
-    }
+  onSuccess: function() {
+    server.start(modules);
+  }
 });
