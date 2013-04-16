ae3af4d2c81fecf024da13c09aeba7837098d411
Mads Mobæk <mads86@gmail.com>
2013-04-16 23:59:28 +0200

    Normalizing tabs -> 2 spaces

--- bin/perspective-api.js
+++ bin/perspective-api.js
@@ -1,24 +1,21 @@
 #!/usr/bin/env node
 
 var Server = require('../lib/server'),
-	Db = require("../lib/db"),
+  Db = require("../lib/db"),
+  config = require("../config.json"),
+  loadPlugins = require("../lib/plugins"),
+  server = new Server(config.server),
+  database = new Db(config.db);
 
-    config = require("../config.json"),
+database.setup().then(function() {
+  server.start();
+  var plugins = loadPlugins(config.plugins, server, database);
+  plugins.forEach(function(plugin) {
+    console.log('Setting up plugin with name: ' + plugin.config.name);
 
-    loadPlugins = require("../lib/plugins"),
-
-    server = new Server(config.server),
-    database = new Db(config.db);
-
-database.setup().then(function(){
-	server.start();
-	var plugins = loadPlugins(config.plugins, server, database);
-	plugins.forEach(function(plugin) {
-        console.log('Setting up plugin with name: ' + plugin.config.name);
-
-		plugin.api.setup();
-	});
+    plugin.api.setup();
+  });
 }).fail(function(error) {
-    console.log(error);
-    process.exit(1);
+  console.log(error);
+  process.exit(1);
 });
--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var assert = require("assert");
 
 describe("something", function() {
-	it("works", function() {
-		"a".should.equal("a");
-	});
+  it("works", function() {
+    "a".should.equal("a");
+  });
 });
