61b1f93b26314826081f9a04fc915782e279e044
Hans Magnus Inderberg <hinderberg@gmail.com>
2013-05-31 12:58:28 +0200

    Added Bunyan for logging

--- bin/perspective-api.js
+++ bin/perspective-api.js
@@ -1,21 +1,23 @@
 #!/usr/bin/env node
 
-var Server = require('../lib/server'),
-  Db = require("../lib/db"),
-  config = require("../config.json"),
-  PluginsLoader = require("../lib/pluginsLoader"),
-  server = new Server(config.server),
-  database = new Db(config.db);
+var Server = require('../lib/server');
+var Db = require("../lib/db");
+var config = require("../config.json");
+var PluginsLoader = require("../lib/pluginsLoader");
+var logger = require("../lib/logger");
+var server = new Server(config.server, logger);
+var database = new Db(config.db, logger);
+
 
 database.setup().then(function() {
   server.start();
-  var pluginsLoader = new PluginsLoader(server, database);
+  var pluginsLoader = new PluginsLoader(server, database, logger);
   var plugins = pluginsLoader.load(config.plugins);
   plugins.forEach(function(plugin) {
-    console.log('Setting up plugin with name: ' + plugin.config.name);
+    logger.info('Setting up plugin with name: ' + plugin.config.name);
     plugin.api.setup();
   });
 }).fail(function(error) {
-  console.log(error);
+  logger.error(error);
   process.exit(1);
 });
