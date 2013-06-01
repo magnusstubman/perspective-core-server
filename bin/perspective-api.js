5161d5ae45997f197ba5abc3c63f5ba3efee0288
Hans Magnus Inderberg <hinderberg@gmail.com>
2013-06-01 12:53:38 +0200

    Instead of re-using the same logger, each module can create their own

--- bin/perspective-api.js
+++ bin/perspective-api.js
@@ -4,20 +4,20 @@ var Server = require('../lib/server');
 var Db = require("../lib/db");
 var config = require("../config.json");
 var PluginsLoader = require("../lib/pluginsLoader");
-var logger = require("../lib/logger");
-var server = new Server(config.server, logger);
-var database = new Db(config.db, logger);
+var apiLogger = require('../lib/apiLogger');
+var server = new Server(config.server);
+var database = new Db(config.db);
 
 
 database.setup().then(function() {
   server.start();
-  var pluginsLoader = new PluginsLoader(server, database, logger);
+  var pluginsLoader = new PluginsLoader(server, database);
   var plugins = pluginsLoader.load(config.plugins);
   plugins.forEach(function(plugin) {
-    logger.info('Setting up plugin with name: ' + plugin.config.name);
+    apiLogger.info('Setting up plugin with name: ' + plugin.config.name);
     plugin.api.setup();
   });
 }).fail(function(error) {
-  logger.error(error);
+  apiLogger.error(error);
   process.exit(1);
 });
