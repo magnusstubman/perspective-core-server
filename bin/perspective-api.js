66b8cc9e357160c964f704100112a222abdcebe7
Hans Magnus Inderberg <hinderberg@gmail.com>
2013-09-17 23:07:45 +0200

    Refactoring how the db api is exposed

--- bin/perspective-api.js
+++ bin/perspective-api.js
@@ -1,17 +1,15 @@
 #!/usr/bin/env node
 
-var Server = require('../lib/server');
-var Db = require("../lib/db");
+var server = require('../lib/server');
+var db = require("../lib/db");
 var config = require("../config.json");
 var PluginsLoader = require("../lib/pluginsLoader");
 var apiLogger = require('../lib/apiLogger');
-var server = new Server(config.server);
-var database = new Db(config.db);
 
 
-database.setup().then(function() {
-  server.start();
-  var pluginsLoader = new PluginsLoader(server, database);
+db(config.db).then(function(dbAPI) {
+  var serverAPI = server(config.server);
+  var pluginsLoader = new PluginsLoader(serverAPI, dbAPI);
   var plugins = pluginsLoader.load(config.plugins);
   plugins.forEach(function(plugin) {
     apiLogger.info('Setting up plugin with name: ' + plugin.config.name);
