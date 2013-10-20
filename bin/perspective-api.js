0cbfdfe93b62e67ffedfefb9976e4c7258184a39
Hans Magnus Inderberg <hinderberg@gmail.com>
2013-10-20 22:18:43 +0200

    Wrapped api in object to make it possible to return server

--- bin/perspective-api.js
+++ bin/perspective-api.js
@@ -1,14 +1,19 @@
 #!/usr/bin/env node
 
 var server = require('../lib/server');
+var webSocketServer = require('../lib/webSocketServer');
 var db = require("../lib/db");
 var config = require("../config.json");
 var PluginsLoader = require("../lib/pluginsLoader");
 var apiLogger = require('../lib/apiLogger');
 
 
-db(config.db).then(function(createRepository) {
-  var pluginsLoader = new PluginsLoader(server(config.server), createRepository);
+db(config.db).then(function(db) {
+
+  var httpServer = server(config.server);
+  var wsServer = webSocketServer(httpServer.server, config.server);
+
+  var pluginsLoader = new PluginsLoader(httpServer, wsServer, db);
   var plugins = pluginsLoader.load(config.plugins);
   plugins.forEach(function(plugin) {
     apiLogger.info('Setting up plugin with name: ' + plugin.config.name);
